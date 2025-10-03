c9ec84b53398d298bd4a9561685caee4dd75bdfb
Nathaniel Tampus <nate@mugnavo.com>
2025-10-03 14:05:22 +0800

    add @netlify/vite-plugin-tanstack-start

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   server: {
@@ -12,6 +13,7 @@ export default defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tanstackStart(),
+    netlify(),
     viteReact(),
   ],
 })
